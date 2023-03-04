b48e16e24c77a6eb01b6e3cbbb591e9330b0b146
Sergio-Luis20 <sergioluisdasilvaruiz05@gmail.com>
2023-03-03 23:45:20 -0300

    revert

--- src/main/java/net/stardust/comlib/RequestMapper.java
+++ src/main/java/net/stardust/comlib/RequestMapper.java
@@ -1,39 +1,10 @@
 package net.stardust.comlib;
 
 import java.io.Serializable;
-import java.util.Objects;
 
-public abstract class RequestMapper {
-
-    protected final String id;
-
-    public RequestMapper(String id) {
-        this.id = Objects.requireNonNull(id);
-    }
+@FunctionalInterface
+public interface RequestMapper {
 
     public abstract <T extends Serializable, U extends Serializable> Response<T> handle(Request<U> request) throws Exception;
-
-    public final String getID() {
-        return id;
-    }
-
-    @Override
-    public final boolean equals(Object o) {
-        if(o == null) {
-            return false;
-        }
-        if(o == this) {
-            return true;
-        }
-        if(o instanceof RequestMapper mapper) {
-            return id.equals(mapper.id);
-        }
-        return false;
-    }
-
-    @Override
-    public final int hashCode() {
-        return id.hashCode();
-    }
 
 }
