9c3a1f393a820fa4c4b45dda1001d7c228ec73c1
Sergio-Luis20 <sergioluisdasilvaruiz05@gmail.com>
2023-04-10 04:40:13 -0300

    fixed bug at socket listener task initialization

--- src/main/java/net/stardust/comlib/SocketListener.java
+++ src/main/java/net/stardust/comlib/SocketListener.java
@@ -21,36 +21,48 @@ public class SocketListener extends RequestListener {
     }
 
     @Override
-    public final void run() {
+    public void start() {
         if(!handler.isConnected()) {
-            String message = "cannot init socket listener because socket isn't connected";
+            String message = "Cannot init socket listener because socket is not connected";
             SocketException socketException = new SocketException(message);
             throw new RuntimeException(socketException);
         }
         try {
             ObjectOutputStream output = handler.getOutputStream();
-            ObjectInputStream input = handler.getInputStream();
             output.writeObject("register=" + id);
             output.flush();
-            if(input.readBoolean()) {
-                while(!handler.isClosed()) {
-                    Object obj = input.readObject();
-                    if(obj instanceof Request<?> request) {
-                        Response<?> response;
-                        try {
-                            response = mapper.handle(request);
-                        } catch(MappingException e) {
-                            response = Response.emptyResponse(ResponseStatus.BAD_REQUEST);
-                            e.printStackTrace();
-                        } catch(Exception e) {
-                            response = Response.emptyResponse(ResponseStatus.INTERNAL_SERVER_ERROR);
-                            e.printStackTrace();
-                        }
-                        output.writeObject(response);
-                        output.flush();
-                    } else {
-                        throw new IOException(obj + " from " + obj.getClass() + " is not an implementation of " + Request.class);
+            if(handler.getInputStream().readBoolean()) {
+                super.start();
+            } else {
+                registrationFailed();
+            }
+        } catch(IOException e) {
+            catchIOException(e);
+        }
+    }
+
+    @Override
+    public final void run() {
+        try {
+            ObjectOutputStream output = handler.getOutputStream();
+            ObjectInputStream input = handler.getInputStream();
+            while(!handler.isClosed()) {
+                Object obj = input.readObject();
+                if(obj instanceof Request<?> request) {
+                    Response<?> response;
+                    try {
+                        response = mapper.handle(request);
+                    } catch(MappingException e) {
+                        response = Response.emptyResponse(ResponseStatus.BAD_REQUEST);
+                        e.printStackTrace();
+                    } catch(Exception e) {
+                        response = Response.emptyResponse(ResponseStatus.INTERNAL_SERVER_ERROR);
+                        e.printStackTrace();
                     }
+                    output.writeObject(response);
+                    output.flush();
+                } else {
+                    throw new IOException(obj + " from " + obj.getClass() + " is not an implementation of " + Request.class);
                 }
             }
         } catch(IOException e) {
@@ -61,5 +73,6 @@ public class SocketListener extends RequestListener {
     }
 
     protected void catchIOException(IOException e) {}
+    protected void registrationFailed() {}
 
 }
